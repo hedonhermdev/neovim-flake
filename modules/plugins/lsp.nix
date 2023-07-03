a3c03e417955ef2909837633fa956bb48f80ff6b
Tirth Jain <jaintirth24@gmail.com>
2023-07-03 04:45:48 +0000

    switch to luasnip (fix)

--- modules/plugins/lsp.nix
+++ modules/plugins/lsp.nix
@@ -26,12 +26,19 @@ with builtins;
 
   vim.luaConfigRC = ''
       -- Set up nvim-cmp.
-    local cmp = require'cmp'
+    local cmp = require('cmp')
+    local luasnip = require('luasnip')
+
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
 
     cmp.setup({
       snippet = {
         expand = function(args)
-          require('luasnip').lsp_expand(args.body)
+          luasnip.lsp_expand(args.body)
         end,
       },
       window = {
